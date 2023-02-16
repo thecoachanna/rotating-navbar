2151d1468902ade0f34127abaf003e01709b897e
Anna Nettles <99093454+thecoachanna@users.noreply.github.com>
2023-02-16 11:30:02 -0500

    completed CSS styling and working

--- .history/script_20230216112853.js
+++ .history/script_20230216112853.js
@@ -1,7 +1,8 @@
 const open = document.getElementById('open')
 const close = document.getElementById('close')
-const container = document.querySelector('.container')
+const container = document.querySelectorAll('.container')
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
+
